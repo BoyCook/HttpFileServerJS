8b552b05a6b1cc4ae64010884b2bb81b5afc50db
Craig Cook <boycook@me.com>
2013-03-27 11:44:15 +0000

    Added more parser tests

--- test/spec/ParserSpec.js
+++ test/spec/ParserSpec.js
@@ -14,4 +14,23 @@ describe('Parser', function () {
             json.should.eql({ "item": ['1', '2', '3'] });
         });
     });
+    describe('#resourceJSON', function () {
+        it('should list as JSON properly', function () {
+            var json = Parser.resourceJSON({data: ['1', '2', '3']});
+            json.should.eql(JSON.stringify({ "resources": ['1', '2', '3'] }));
+        });
+    });
+    describe('#resourceXML', function () {
+        it('should list as XML properly', function () {
+            var xml = Parser.resourceXML({data: ['1', '2', '3']});
+            xml.should.eql('<resources><resource>1</resource><resource>2</resource><resource>3</resource></resources>');
+        });
+    });
+    describe('#resourceHTML', function () {
+        it('should list as HTML properly', function () {
+            var expected = '<div><div><a href="1">1</a></div><div><a href="2">2</a></div><div><a href="3">3</a></div></div>';
+            var html = Parser.resourceHTML({data: ['1', '2', '3']});
+            html.should.eql(expected);
+        });
+    });
 });
