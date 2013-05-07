72e4f4069441b8533d48cf9d800a69f3ae4fc3fb
Craig Cook <boycook@me.com>
2013-05-07 20:59:34 +0100

    Added paths to server config

--- serve.js
+++ serve.js
@@ -1,2 +1,9 @@
 var HttpServer = require('./lib/server').HttpServer;
-new HttpServer({port: 8080, baseDir: '.', verbose: true}).start();
+new HttpServer({
+    port: 8080,
+    baseDir: '.',
+    verbose: true,
+    paths: [
+        { path: '/test/:id', params: {makeDir: true}}
+    ]
+}).start();
