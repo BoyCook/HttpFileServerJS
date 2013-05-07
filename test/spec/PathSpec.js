ae3eadd49806a797afd6e29606765bcf0536ec69
Craig Cook <boycook@me.com>
2013-05-07 15:00:17 +0100

    Added paths tests

--- test/spec/PathSpec.js
+++ test/spec/PathSpec.js
@@ -1,34 +1,31 @@
-
 var should = require('should');
 var Path = require('../../index.js').Path;
 
-describe('Path', function() {
+describe('Path', function () {
 
     var path;
     var pathWithParam;
 
-    before(function(){
+    before(function () {
         path = new Path('/users');
         pathWithParam = new Path('/users/:id');
     });
 
-    describe('#match', function() {
-        it('should match path', function(){
+    describe('#match', function () {
+        it('should match path', function () {
             should.exist(path.match('/users'));
         });
 
-        it('should not match invalid path', function(){
+        it('should not match invalid path', function () {
             should.not.exist(path.match('/user'));
         });
 
-        it('should match path with parameter ok', function(){
-            var result = pathWithParam.match('/users/boycook');
-            should.exist(result);
+        it('should match path with parameter ok', function () {
+            should.exist(pathWithParam.match('/users/boycook'));
         });
 
-        it('should not match invalid path with param', function(){
-            var result = pathWithParam.match('/user/boycook');
-            should.not.exist(result);
+        it('should not match invalid path with param', function () {
+            should.not.exist(pathWithParam.match('/user/boycook'));
         });
     });
 });
--- test/spec/PathsSpec.js
+++ test/spec/PathsSpec.js
@@ -1,15 +1,34 @@
-
 var should = require('should');
+var Path = require('../../index.js').Path;
 var Paths = require('../../index.js').Paths;
 
-describe.skip('Paths', function() {
-    it('should match valid path ok', function(){
+describe('Paths', function () {
+    var paths;
 
+    before(function () {
+        var _paths = ['/users', {path: '/users/:id', params: {makeDir: true}}];
+        paths = new Paths({paths: _paths});
     });
 
-    describe('#match', function() {
-        it('should match valid path ok', function(){
+    describe('#match', function () {
+        it('should match path', function () {
+            var result = paths.match('/users');
+            result.should.be.true;
+        });
 
+        it('should not match invalid path', function () {
+            var result = paths.match('/user');
+            result.should.be.false;
+        });
+
+        it('should match path with parameter ok', function () {
+            var result = paths.match('/users/boycook');
+            result.should.be.true;
+        });
+
+        it('should not match invalid path with param', function () {
+            var result = paths.match('/user/boycook');
+            result.should.be.false;
         });
     });
 });
