c64b25db03a029e1037e7d969b99bf742d6643b2
Craig Cook <boycook@me.com>
2013-05-07 14:35:40 +0100

    Got paths tests working

--- test/spec/PathSpec.js
+++ test/spec/PathSpec.js
@@ -2,22 +2,33 @@
 var should = require('should');
 var Path = require('../../index.js').Path;
 
-describe.skip('Path', function() {
+describe('Path', function() {
 
     var path;
+    var pathWithParam;
 
     before(function(){
-        path = new Path('/users/:id');
-    });
-
-    it('should match plain path ok', function(){
-        var result = path.match('/users/boycook');
-        result.should.bt.true;
+        path = new Path('/users');
+        pathWithParam = new Path('/users/:id');
     });
 
     describe('#match', function() {
-        it('should match valid path ok', function(){
+        it('should match path', function(){
+            should.exist(path.match('/users'));
+        });
 
+        it('should not match invalid path', function(){
+            should.not.exist(path.match('/user'));
+        });
+
+        it('should match path with parameter ok', function(){
+            var result = pathWithParam.match('/users/boycook');
+            should.exist(result);
+        });
+
+        it('should not match invalid path with param', function(){
+            var result = pathWithParam.match('/user/boycook');
+            should.not.exist(result);
         });
     });
 });
